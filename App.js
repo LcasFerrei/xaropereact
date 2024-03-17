46f783f8e2421ff53fb489ceb251d9f6c0738a2e
Lucas Ferreira <137240081+LcasFerrei@users.noreply.github.com>
2024-03-17 13:09:47 -0300

    add

---
+++ App.js
@@ -0,0 +1,29 @@
+import AppRoutes from './routes';
+
+/*function App() {
+  return (
+      <div>
+          {/* Componentes */
+          /*<Header />
+          <Banner />
+          <h1>A DOSE CERTA DE CONHECIMENTO PARA SUA JORNADA NA PROGRAMAÇÃO!</h1>
+    <p class="sub">Consuma abaixo algumas pílulas de conhecimento para inicar seu aprendizado.</p>
+          <Playervideo />
+          <Subtitulo />
+
+          <Barrinha />
+          <Footer />
+      </div>
+    
+  );
+}*/
+
+
+function App(){
+  return(
+    <AppRoutes />
+  )
+}
+
+export default App;
+
